db0c690da7585d0f0b83fd00ee66ce28a92501d4
Deepika <bhuvanadeepika017@gmail.com>
2020-09-18 20:49:18 +0530

    added app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 8080;
+const { MONGOURI } = require('./keys');
+
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected with mongo');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('err in connecting', err);
+})
+
+require('./models/user');
+require('./models/post');
+
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+
+app.listen(PORT, () => console.log("Server is listening on ", PORT));
